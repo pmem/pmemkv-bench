9991189b2a0cb0f5fd3f149a53cf5d70c7fa68ac
Rob Dickinson <robfromboulder@gmail.com>
2018-07-11 15:29:56 -0600

    Add C++ baseline

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ reset:
 	rm -rf /dev/shm/pmemkv /tmp/pmemkv
 
 clean: reset
-	rm -rf ./java/*.class
+	rm -rf ./cpp/*.bin ./java/*.class
 
 baseline_c: clean
 	cd c
@@ -12,7 +12,8 @@ baseline_c: clean
 
 baseline_cpp: clean
 	cd cpp
-	echo 'Build and run baseline.cc'
+	g++ baseline.cc -o baseline.bin /usr/local/lib/libpmemkv.so -I/usr/local/include -O2 -std=c++11 -DOS_LINUX -fno-builtin-memcmp -march=native -ldl -lpthread
+	PMEM_IS_PMEM_FORCE=1 ./baseline.bin
 
 baseline_java: clean
 	cd java
--- cpp/baseline.cc
+++ cpp/baseline.cc
@@ -1,1 +1,84 @@
-/* Add C++ baseline implementation */
+#include <iostream>
+#include <sys/time.h>
+#include <vector>
+#include "libpmemkv.h"
+
+#define LOG(msg) std::cout << msg << "\n"
+
+using namespace pmemkv;
+
+static const int COUNT = 1000000;
+static const string PATH = "/dev/shm/pmemkv";
+
+static double current_seconds() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec) + (double) (tv.tv_usec) / 1000000;
+}
+
+struct EachCallbackContext {
+    unsigned long failures;
+};
+
+void test_engine(const string engine, const std::vector<string> keys, const string value) {
+    LOG("\nTesting " << engine << " for " + to_string(keys.size()) << " keys, value size is "
+                     << value.length() << "...\n");
+    std::remove(PATH.c_str());
+    KVEngine* kv = KVEngine::Open(engine, PATH, 1024 * 1024 * 1024);
+
+    // Test put operation
+    LOG("Put (sequential)");
+    auto started = current_seconds();
+    for (int i = 0; i < keys.size(); i++) {
+        if (kv->Put(keys[i], value) != OK) {
+            std::cout << "Out of space at key " << to_string(i) << "\n";
+            exit(-42);
+        }
+    }
+    LOG("   in " << current_seconds() - started << " sec");
+
+    // Test get operation
+    LOG("Get (sequential)");
+    int failures = 0;
+    started = current_seconds();
+    for (int i = 0; i < keys.size(); i++) {
+        string value;
+        if (kv->Get(keys[i], &value) != OK) failures++;
+    }
+    LOG("   in " << current_seconds() - started << " sec, failures=" + to_string(failures));
+
+    // Test exists operation
+    LOG("Exists (sequential)");
+    failures = 0;
+    started = current_seconds();
+    for (int i = 0; i < keys.size(); i++) {
+        if (kv->Exists(keys[i]) != OK) failures++;
+    }
+    LOG("   in " << current_seconds() - started << " sec, failures=" + to_string(failures));
+
+    // Test each operation
+    LOG("Each (natural)");
+    EachCallbackContext cxt = {keys.size()};
+    auto cb = [](void* context, int32_t keybytes, int32_t valuebytes, const char* key, const char* value) {
+        ((EachCallbackContext*) context)->failures--;
+    };
+    started = current_seconds();
+    kv->Each(&cxt, cb);
+    LOG("   in " << current_seconds() - started << " sec, failures=" + to_string(cxt.failures));
+
+    delete kv;
+}
+
+int main() {
+    // format all keys in advance
+    std::vector<string> keys;
+    for (auto i = 0; i < COUNT; i++) keys.push_back(to_string(i));
+
+    // test all engines for all keys & values
+    test_engine("blackhole", keys, "AAAAAAAAAAAAAAAA");
+    test_engine("kvtree2", keys, "AAAAAAAAAAAAAAAA");
+    test_engine("btree", keys, "AAAAAAAAAAAAAAAA");
+
+    LOG("\nFinished!\n");
+    return 0;
+}
