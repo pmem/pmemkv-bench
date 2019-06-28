b01a10332f4b7105139090366c801990aad00929
Anna Marcinkowska <anna.marcinkowska@intel.com>
2019-06-28 19:02:54 +0200

    Update examples

--- nodejs/example.js
+++ nodejs/example.js
@@ -1,27 +1,27 @@
-const KVEngine = require('../../pmemkv-nodejs/lib/kv_engine');
+const Database = require('../../pmemkv-nodejs/lib/database');
 
 function assert(condition) {
     if (!condition) throw new Error('Assert failed');
 }
 
 console.log('Starting engine');
-const kv = new KVEngine('vsmap', '{"path":"/dev/shm/"}');
+const db = new Database('vsmap', '{"path":"/dev/shm", "size":1073741824}');
 
 console.log('Putting new key');
-kv.put('key1', 'value1');
-assert(kv.count === 1);
+db.put('key1', 'value1');
+assert(db.count_all === 1);
 
 console.log('Reading key back');
-assert(kv.get('key1') === 'value1');
+assert(db.get('key1') === 'value1');
 
 console.log('Iterating existing keys');
-kv.put('key2', 'value2');
-kv.put('key3', 'value3');
-kv.all((k) => console.log(`  visited: ${k}`));
+db.put('key2', 'value2');
+db.put('key3', 'value3');
+db.get_keys((k) => console.log(`  visited: ${k}`));
 
 console.log('Removing existing key');
-kv.remove('key1');
-assert(!kv.exists('key1'));
+db.remove('key1');
+assert(!db.exists('key1'));
 
 console.log('Stopping engine');
-kv.stop();
+db.stop();
--- ruby/example.rb
+++ ruby/example.rb
@@ -1,27 +1,27 @@
-require '../../pmemkv-ruby/lib/pmemkv/kv_engine'
+require '../../pmemkv-ruby/lib/pmemkv/database'
 
 def assert(condition)
   raise RuntimeError.new('Assert failed') unless condition
 end
 
 puts 'Starting engine'
-kv = KVEngine.new('vsmap', '{"path":"/dev/shm/"}')
+db = Database.new('vsmap', "{\"path\":\"/dev/shm\",\"size\":1073741824}")
 
 puts 'Putting new key'
-kv.put('key1', 'value1')
-assert kv.count == 1
+db.put('key1', 'value1')
+assert db.count_all == 1
 
 puts 'Reading key back'
-assert kv.get('key1').eql?('value1')
+assert db.get('key1').eql?('value1')
 
 puts 'Iterating existing keys'
-kv.put('key2', 'value2')
-kv.put('key3', 'value3')
-kv.all_strings {|k| puts "  visited: #{k}"}
+db.put('key2', 'value2')
+db.put('key3', 'value3')
+db.get_keys {|k| puts "  visited: #{k}"}
 
 puts 'Removing existing key'
-kv.remove('key1')
-assert !kv.exists('key1')
+db.remove('key1')
+assert !db.exists('key1')
 
 puts 'Stopping engine'
-kv.stop
+db.stop
