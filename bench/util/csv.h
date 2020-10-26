d803c16d781f712996633587bb4028965fe1f8ff
Pawel Karczewski <pawel.karczewski@intel.com>
2020-10-26 16:25:16 +0100

    db_bench: Refactor internal Benchmark object loop and logging to CSV
    Move running benchmarks in loop from Benchmark object to main and change
    logging from printing all over the code to Logger class, which  prints
    csv

---
+++ bench/util/csv.h
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: Apache-2.0
+/* Copyright 2020, Intel Corporation */
+
+#pragma once
+
+#include <map>
+#include <ostream>
+#include <set>
+#include <string>
+#include <iostream>
+
+class CSV
+{
+private:
+
+	std::map<std::string, std::map<std::string, std::string>> data_matrix;
+	std::set<std::string> column_index;
+	std::string id_name;
+
+public:
+	CSV(std::string id_collumn_name) : id_name(id_collumn_name){};
+	void insert(std::string row, std::string column, std::string data)
+	{
+		column_index.insert(column);
+		data_matrix[row][column] = data;
+	}
+
+	void insert(std::string row, std::string column, const char *data)
+	{
+		insert(row, column, std::string(data));
+	}
+
+	template <typename T>
+	void insert(std::string row, std::string column, T data)
+	{
+		insert(row, column, std::to_string(data));
+	}
+
+	void print()
+	{
+		// Print first column name
+		std::cout<< id_name;
+
+		for( auto &column: column_index)
+		{
+			std::cout << "," << column;
+		}
+		std::cout << "\r\n" << std::flush;
+
+		for( auto &row: data_matrix)
+		{
+			std::cout << row.first;
+			for(auto &column: column_index)
+			{
+				std::cout << "," << data_matrix[row.first][column];
+			}
+			std::cout << "\r\n" << std::flush;
+		}
+	}
+};
+
+
