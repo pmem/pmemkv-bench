0da3c06a9a1031994b45247e3c0c41a305580acd
Pawel Karczewski <pawel.karczewski@intel.com>
2021-02-02 17:03:24 +0100

    Resolve git keywords to commit sha
    For proper data postprocessing it's needed to change commit
    identifiers to sha - e.g. HEAD -> commit sha-1

--- run_benchmark.py
+++ run_benchmark.py
@@ -32,6 +32,7 @@ class Repository:
         self.commit = config["commit"]
         self.clone()
         self.checkout()
+        config["commit"] = self._resolve_sha()
 
     def __str__(self):
         return f"{self.url} in {self.path}"
@@ -45,6 +46,17 @@ class Repository:
     def clone(self):
         self.logger.info(f"Cloning repository: {self.url}")
         subprocess.run("git clone".split() + [self.url, self.path], check=True)
+
+    def _resolve_sha(self):
+        rev_parsed_commit = subprocess.run(
+            ["git", "rev-parse", self.commit],
+            cwd=self.path,
+            capture_output=True,
+            check=True,
+            universal_newlines=True,
+        ).stdout
+        self.logger.info(f"Commit sha: {rev_parsed_commit}")
+        return rev_parsed_commit
 
 
 class CmakeProject:
