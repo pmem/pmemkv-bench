a18cc0d3861aecfe175637f12c56082d8560ebb7
Pawel Karczewski <pawel.karczewski@intel.com>
2021-02-02 17:09:12 +0100

    Add supprt for running multiple test cases for one build
    Instead of passing JSON, user may pass python script with implemented method generate(), which returns
    dictionary. Such dictionary is validated against json schema file. To do so, build and benchmark
    configurations was split into two files.
    
    generate_obj_based_scope.py may be run as standalone application, which
    prints configuration json (also validated against schema) to stdout. Such once generated json may be saved
    and passed to run_benchmark.py.
    
    ref #45

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+#
+# SPDX-License-Identifier: Apache-2.0
+# Copyright 2021, Intel Corporation
+
 import os, sys
 import pytest
 import json
@@ -8,7 +13,15 @@ sys.path.append(project_path)
 import run_benchmark as rb
 
 
+def create_config_file(configuration):
+    tf = tempfile.NamedTemporaryFile(suffix=".json", mode="w", delete=False)
+    json.dump(configuration, tf)
+    tf.close()
+    return tf
+
+
 def test_help():
+    """ Simple sanity check for -h option of run_benchmark.py. """
     sys.argv = ["dummy.py", "-h"]
     with pytest.raises(SystemExit) as e:
         result = rb.main()
@@ -17,23 +30,26 @@ def test_help():
 
 
 def test_json():
-    test_configuration = {
+    """Basic integration test for run_benchmark.py. It runs full
+    benchmarking process for arbitrarily chosen parameters.
+    """
+
+    build_configuration = {
         "db_bench": {
-            "repo_url": f"{project_path}",
+            "repo_url": project_path,
             "commit": "HEAD",
-            "env": {"PMEM_IS_PMEM_FORCE": "1"},
-            "params": [
-                f"--db={os.getenv('TEST_PATH', '/dev/shm')}",
-                "--db_size_in_gb=1",
-                "--num=100",
-            ],
+            "env": {},
         },
         "pmemkv": {
             "repo_url": "https://github.com/pmem/pmemkv.git",
             "commit": "HEAD",
             "cmake_params": [
                 "-DCMAKE_BUILD_TYPE=Release",
+                "-DENGINE_CMAP=1",
+                "-DENGINE_CSMAP=1",
                 "-DENGINE_RADIX=1",
+                "-DENGINE_STREE=1",
+                "-DENGINE_ROBINHOOD=1",
                 "-DBUILD_JSON_CONFIG=1",
                 "-DCXX_STANDARD=20",
                 "-DBUILD_TESTS=OFF",
@@ -55,11 +71,37 @@ def test_json():
             "env": {"CC": "gcc", "CXX": "g++"},
         },
     }
-    tf = tempfile.NamedTemporaryFile(suffix=".json", mode="w", delete=False)
-    json.dump(test_configuration, tf)
-    print(tf.name)
-    sys.argv = ["dummy.py", tf.name]
-    tf.close()
+
+    benchmark_configuration = [
+        {
+            "env": {"PMEM_IS_PMEM_FORCE": "1"},
+            "params": {
+                "--db": os.getenv("TEST_PATH", "/dev/shm/pmemkv"),
+                "--db_size_in_gb": "1",
+                "--benchmarks": "fillrandom",
+                "--engine": "cmap",
+                "--num": "100",
+                "--value_size": "8",
+                "--threads": "2",
+            },
+        },
+        {
+            "env": {},
+            "params": {
+                "--db": os.getenv("TEST_PATH", "/dev/shm/pmemkv"),
+                "--db_size_in_gb": "2",
+                "--benchmarks": "fillseq",
+                "--engine": "radix",
+                "--num": "100",
+                "--value_size": "1024",
+                "--threads": "1",
+            },
+        },
+    ]
+
+    build_config_file = create_config_file(build_configuration)
+    test_config_file = create_config_file(benchmark_configuration)
+    sys.argv = ["dummy.py", build_config_file.name, test_config_file.name]
     try:
         result = rb.main()
     except Exception as e:
