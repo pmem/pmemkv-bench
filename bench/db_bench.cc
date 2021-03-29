c5979be0da89331ec2197cf7f4f93b1e30172751
Łukasz Stolarczuk <lukasz.stolarczuk@intel.com>
2021-03-29 17:31:05 +0200

    bench: fix and extend error print messages

--- bench/db_bench.cc
+++ bench/db_bench.cc
@@ -545,7 +545,7 @@ public:
 	      readwrites_(FLAGS_reads < 0 ? FLAGS_num : FLAGS_reads), logger(logger), n(num_threads),
 	      name(name), engine(engine)
 	{
-		fprintf(stderr, "running %s \n", name.ToString().c_str());
+		fprintf(stderr, "Running %s\n", name.ToString().c_str());
 		bool fresh_db = false;
 
 		if (name == Slice("fillseq")) {
@@ -776,7 +776,7 @@ private:
 
 		if (s != pmem::kv::status::OK) {
 			fprintf(stderr,
-				"Cannot start engine (%s) for path (%s) with %i GB capacity\n%s",
+				"Cannot start engine (%s) for path (%s) with %i GB capacity\nError: %s",
 				engine, FLAGS_db, FLAGS_db_size_in_gb, pmem::kv::errormsg().c_str());
 			exit(-42);
 		}
@@ -819,7 +819,8 @@ private:
 			s = inserter.commit();
 			thread->stats.FinishedSingleOp();
 			if (s != pmem::kv::status::OK) {
-				fprintf(stdout, "Commit failed at batch %i\n", n);
+				fprintf(stdout, "Commit failed at batch %i\nError: %s\n", n,
+					pmem::kv::errormsg().c_str());
 				exit(1);
 			}
 		}
@@ -936,7 +937,8 @@ private:
 			written++;
 
 			if (s != pmem::kv::status::OK) {
-				fprintf(stderr, "Put error\n");
+				fprintf(stderr, "Put error for key '%s' (%s)\n", key.ToString().c_str(),
+					pmem::kv::errormsg().c_str());
 				exit(1);
 			}
 			bytes += key.size() + value_size_;
@@ -982,7 +984,8 @@ private:
 				if (s == pmem::kv::status::OK) {
 					found++;
 				} else if (s != pmem::kv::status::NOT_FOUND) {
-					fprintf(stderr, "get error\n");
+					fprintf(stderr, "Get error for key '%s' (%s)\n",
+						key.ToString().c_str(), pmem::kv::errormsg().c_str());
 				}
 
 				bytes += value.length() + key.size();
@@ -995,7 +998,8 @@ private:
 				pmem::kv::status s =
 					kv_->put(key.ToString(), gen.Generate(value_size_).ToString());
 				if (s != pmem::kv::status::OK) {
-					fprintf(stderr, "put error\n");
+					fprintf(stderr, "Put error for key '%s' (%s)\n",
+						key.ToString().c_str(), pmem::kv::errormsg().c_str());
 					exit(1);
 				}
 				bytes += key.size() + value_size_;
